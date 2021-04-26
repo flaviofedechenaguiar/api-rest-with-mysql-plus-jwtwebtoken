84881854a17c978f3b33591acef560378af15446
Flavio Fedechen Aguiar <flaviofedechenaguiar@gmail.com>
2021-04-25 21:28:48 -0300

    added user model; user/auth routes

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const connection = require('./database/database');
+const cors = require(cors);
+
 
 const Game = require('./models/game.js');
+const User = require('./models/user.js'); 
 
+let { Op } = require('sequelize');
+
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -66,6 +72,50 @@ app.delete('/game/:id', async (req, res) => {
         res.sendStatus(200);
     } catch (err) {
         res.sendStatus(404);
+    }
+});
+
+app.post('/user', async (req, res) => {
+    let { name, email, password } = req.body;
+    if ((name == '' || !name) || (email == '' || !email) || (password == '' || !password)) {
+        res.sendStatus(400);
+    } else {
+        try {
+            User.create({ name, email, password });
+            res.sendStatus(200);
+        } catch (err) {
+            res.sendStatus(404);
+        }
+    }
+});
+
+app.post('/auth', async (req, res) => {
+    let { email, password } = req.body;
+    if ((email == '' || !email) || (password == '' || !password)) {
+        res.sendStatus(400);
+    } else {
+        try {
+            let user = await User.findOne({
+                where: {
+                    email: {
+                        [Op.eq]: email
+                    }
+                }
+            });
+
+            if (user) {
+                if (user.password == password) {
+                    res.json('FAKE TOKEN').status(200);;
+                } else {
+                    res.json({ error: 'Senha incorreta' }).status(401);
+                }
+            } else {
+                res.json({ error: 'Email não encontrado' }).status(404);
+            }
+
+        } catch (err) {
+            res.sendStatus(404);
+        }
     }
 });
 
